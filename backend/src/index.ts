78c78849d23e975e6bf7771a94cc9526d3d06e8e
derick1129 <derickjohn1129@gmail.com>
2026-03-06 15:34:42 +0530

    initialised express app'

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import authRouter from './routes/auth.route';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
+
+app.use("/api/auth", authRouter);
+
+export default app;
